6f537fd7d92278436324b3a96956dc1a3031578f
Alex Osborne <aosborne@nla.gov.au>
2014-03-05 17:03:54 +1100

    Implement BlobStoreVfs

--- src/doss/nfs/BlobStoreVFS.java
+++ src/doss/nfs/BlobStoreVFS.java
@@ -1,6 +1,8 @@
 package doss.nfs;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,27 +18,34 @@ import org.dcache.nfs.vfs.Stat;
 import org.dcache.nfs.vfs.VirtualFileSystem;
 import org.dcache.nfs.vfs.Stat.Type;
 
+import doss.Blob;
+import doss.BlobStore;
+import doss.NoSuchBlobException;
+import static org.dcache.nfs.v4.xdr.nfs4_prot.*;
+
 public class BlobStoreVFS implements VirtualFileSystem {
 
-	private static final FileHandle rootFileHandle = new FileHandle.FileHandleBuilder().build("/".getBytes()); 
+	private static final String BLOB_PREFIX = "nla.doss-";
+	private static final FileHandle rootFileHandle = new FileHandle.FileHandleBuilder()
+			.build("/".getBytes());
 	private static final Inode rootInode = new Inode(rootFileHandle);
-	
-	@Override
-	public int access(Inode arg0, int arg1) throws IOException {
-		// TODO Auto-generated method stub
-		return 0;
+	private static final byte[] README_TEXT = "This filesystem is a NFS to DOSS gateway.\nAccess invisible files like /123.anyextension to read DOSS blobs.\n".getBytes();
+
+	private final BlobStore blobStore;
+
+	public BlobStoreVFS(BlobStore blobStore) {
+		this.blobStore = blobStore;
 	}
 
 	@Override
-	public nfsace4[] getAcl(Inode arg0) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+	public int access(Inode inode, int mode) throws IOException {
+		return mode & (ACCESS4_READ | ACCESS4_LOOKUP); // everything is
+														// read-only
 	}
 
 	@Override
 	public FsStat getFsStat() throws IOException {
-		// TODO Auto-generated method stub
-		return new FsStat(100000, 0, 0, 0);
+		return new FsStat(10L * 1024 * 1024 * 1024, 0, 0, 0);
 	}
 
 	@Override
@@ -46,29 +55,111 @@ public class BlobStoreVFS implements VirtualFileSystem {
 
 	@Override
 	public Stat getattr(Inode inode) throws IOException {
-		System.out.println("getattr called " + inode);
-		// TODO Auto-generated method stub
+		String path = new String(inode.getFileId());
 		Stat stat = new Stat();
-		stat.setMode(0755 | UnixPermission.S_IFDIR);
+		stat.setGid(0);
+		stat.setUid(0);
+		stat.setNlink(1);
+		if ("/".equals(path)) {
+			stat.setMode(0555 | UnixPermission.S_IFDIR);
+			stat.setSize(0);
+			long now = System.currentTimeMillis();
+			stat.setATime(now);
+			stat.setMTime(now);
+			stat.setCTime(now);
+			stat.setIno(0);
+			stat.setFileid(0);
+		} else if ("/README".equals(path)) {
+			stat.setMode(0444 | UnixPermission.S_IFREG);
+			stat.setSize(README_TEXT.length);
+			stat.setATime(1393987443000L);
+			stat.setMTime(1393987443000L);
+			stat.setCTime(1393987443000L);
+			stat.setIno(-1);
+			stat.setFileid(-1);			
+		} else {
+			stat.setMode(0444 | UnixPermission.S_IFREG);
+			Blob blob = inodeToBlob(inode);
+			if (blob == null) {
+				throw new ChimeraNFSException(nfsstat.NFSERR_NOENT, "no such blob");
+			}
+			stat.setSize(blob.size());
+			stat.setMTime(blob.created().toMillis());
+			stat.setCTime(blob.created().toMillis());
+			stat.setATime(blob.created().toMillis());
+			stat.setIno((int)blob.id());
+			stat.setFileid(blob.id());
+		}		
 		return stat;
 	}
 
 	@Override
-	public boolean hasIOLayout(Inode inode) throws IOException {
-		// TODO what is this?
-		return false;
-	}
-
-	@Override
 	public List<DirectoryEntry> list(Inode inode) throws IOException {
-		return new ArrayList<>(); // always empty
+		List<DirectoryEntry> list = new ArrayList<>();
+		list.add(new DirectoryEntry("README", toInode("/README"),
+				getattr(toInode("/README"))));
+		return list;
 	}
 
 	@Override
-	public Inode lookup(Inode arg0, String path) throws IOException {
-		// TODO Auto-generated method stub
-		System.out.println("lookup " + path);
-		return null;
+	public Inode lookup(Inode parentInode, String path) throws IOException {
+		if (!"/".equals(inodePath(parentInode))) {
+			System.out.println("bad blobid2 " + path + " .. " + parentInode
+					+ " vs " + rootInode);
+			// we don't support directories except for the root
+			throw new ChimeraNFSException(nfsstat.NFSERR_NOENT, "not found");
+		}
+		if (path.equals("README")) {
+			return toInode("/README");
+		}
+		Long blobId = pathToBlobId(path);
+		if (blobId == null) {
+			throw new ChimeraNFSException(nfsstat.NFSERR_NOENT, "bad blobId");
+		}
+		try {
+			blobStore.get(blobId);
+		} catch (NoSuchBlobException e) {
+			throw new ChimeraNFSException(nfsstat.NFSERR_NOENT, "no such blob");
+		}
+		return toInode(BLOB_PREFIX + blobId);
+	}
+
+	private Long pathToBlobId(String path) throws ChimeraNFSException {
+		int dotPos = path.indexOf(".");
+		String id;
+		if (dotPos >= 0) {
+			id = path.substring(0, dotPos);
+		} else {
+			id = path;
+		}
+		try {
+			return Long.parseLong(id);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+
+	private Inode toInode(String path) {
+		return new Inode(new FileHandle.FileHandleBuilder().build(path
+				.getBytes()));
+	}
+
+	private String inodePath(Inode inode) {
+		return new String(inode.getFileId());
+	}
+
+	private Blob inodeToBlob(Inode inode) throws IOException {
+		String path = inodePath(inode);
+		if (path.startsWith(BLOB_PREFIX)) {
+			try {
+				return blobStore.get(Long.parseLong(path.substring(BLOB_PREFIX
+						.length())));
+			} catch (NoSuchBlobException | NumberFormatException e) {
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 
 	@Override
@@ -80,69 +171,109 @@ public class BlobStoreVFS implements VirtualFileSystem {
 	}
 
 	@Override
-	public int read(Inode arg0, byte[] arg1, long arg2, int arg3)
-			throws IOException {
-		// TODO
-		return 0;
+	public int read(Inode inode, byte[] data, long offset, int count)
+			throws IOException {	
+		Blob blob = inodeToBlob(inode);
+		if (blob != null) {
+			return readFromBlob(blob, data, offset, count);
+		} else {
+			return readFromReadme(data, offset, count);
+		}
 	}
-	
+
+	private int readFromBlob(Blob blob, byte[] data, long offset, int count)
+			throws IOException {
+		try (SeekableByteChannel ch = blob.openChannel()) {
+			ByteBuffer buf = ByteBuffer.wrap(data, 0, count);
+			ch.position(offset);
+			ch.read(buf);
+			return buf.position();
+		}
+	}
+
+	private int readFromReadme(byte[] data, long offset, int count) {
+		int nbytes = (int)Math.min(count - offset, README_TEXT.length);
+		System.arraycopy(README_TEXT, (int)offset, data, 0, nbytes);
+		return nbytes;
+	}
+
 	//
 	// Unsupported operations
 	//
-	
+
 	@Override
-	public String readlink(Inode arg0) throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_INVAL, "not a symbolic link");		
-	}
-	
-	@Override
-	public Inode mkdir(Inode arg0, String arg1, int arg2, int arg3, int arg4)
-			throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+	public boolean hasIOLayout(Inode inode) throws IOException {
+		return false; // all data is local to the server
 	}
 
 	@Override
-	public void move(Inode arg0, String arg1, Inode arg2, String arg3)
-			throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+	public nfsace4[] getAcl(Inode inode) throws IOException {
+		return new nfsace4[0];
 	}
-	
+
+	@Override
+	public String readlink(Inode arg0) throws IOException {
+		throw new ChimeraNFSException(nfsstat.NFSERR_INVAL,
+				"not a symbolic link");
+	}
+
+	@Override
+	public Inode mkdir(Inode arg0, String arg1, int arg2, int arg3, int arg4)
+			throws IOException {
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
+	}
+
+	@Override
+	public boolean move(Inode arg0, String arg1, Inode arg2, String arg3)
+			throws IOException {
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
+	}
+
 	@Override
 	public Inode link(Inode arg0, Inode arg1, String arg2, int arg3, int arg4)
 			throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
 	}
-	
+
 	@Override
 	public Inode create(Inode arg0, Type arg1, String arg2, int arg3, int arg4,
 			int arg5) throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
 	}
 
 	@Override
 	public void remove(Inode arg0, String arg1) throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
 	}
 
 	@Override
 	public void setAcl(Inode arg0, nfsace4[] arg1) throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
 	}
 
 	@Override
 	public void setattr(Inode arg0, Stat arg1) throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
 	}
 
 	@Override
 	public Inode symlink(Inode arg0, String arg1, String arg2, int arg3,
 			int arg4, int arg5) throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
 	}
 
 	@Override
 	public int write(Inode arg0, byte[] arg1, long arg2, int arg3)
 			throws IOException {
-		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS, "doss-nfsd does not support writing");
+		throw new ChimeraNFSException(nfsstat.NFSERR_ROFS,
+				"doss-nfsd does not support writing");
 	}
 }
--- src/doss/nfs/Main.java
+++ src/doss/nfs/Main.java
@@ -2,12 +2,13 @@ package doss.nfs;
 
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.dcache.nfs.ExportFile;
 import org.dcache.nfs.v3.MountServer;
-import org.dcache.nfs.v3.NfsServerV3;
+import org.dcache.nfs.v4.DeviceManager;
 import org.dcache.nfs.v4.MDSOperationFactory;
 import org.dcache.nfs.v4.NFSServerV41;
 import org.dcache.nfs.v4.NfsIdMapping;
@@ -18,41 +19,59 @@ import org.dcache.xdr.OncRpcProgram;
 import org.dcache.xdr.OncRpcSvc;
 import org.dcache.xdr.OncRpcSvcBuilder;
 import org.dcache.xdr.RpcDispatchable;
-import org.dcache.xdr.portmap.OncRpcEmbeddedPortmap;
+
+import doss.BlobStore;
+import doss.local.LocalBlobStore;
 
 public class Main {
-	public static void main(String[] args) throws IOException, OncRpcException, InterruptedException {
-		// virtual file system
-		VirtualFileSystem fs = new BlobStoreVFS();
-		
-		// exports config file
-		URL exportsUrl = Main.class.getClassLoader().getResource("doss/nfs/exports");
-		if (exportsUrl == null) {
-			throw new RuntimeException("doss/nfs/exports not found on classpath");
+	public static void main(String[] args) throws IOException, OncRpcException,
+			InterruptedException {
+		if (args.length < 1) {
+			System.err.println("Usage: doss-nfsd blobstore-path");
+			System.exit(1);
 		}
-		ExportFile exports = new ExportFile(exportsUrl);
-		
-		// NFS 4 server
-		MDSOperationFactory opfac = new MDSOperationFactory();
-		NfsIdMapping idMap = new SimpleIdMap();
-		NFSServerV41 nfs4 = new NFSServerV41(opfac, null, fs, idMap, exports);
-		
-		// mountd
-		MountServer mountd = new MountServer(exports, fs);
 
-		// RPC server
-		OncRpcSvc rpcSvc = new OncRpcSvcBuilder().withPort(1234).withTCP().build();
-		Map<OncRpcProgram, RpcDispatchable> services = new HashMap<>();
-		rpcSvc.register(new OncRpcProgram(100003, 4), nfs4);
-		rpcSvc.register(new OncRpcProgram(100005, 3), mountd);
-		rpcSvc.setPrograms(services );
-		rpcSvc.start();
-		
-		try {
-			// hang around for a while
-			Thread.sleep(100000);
-		} finally {
-			rpcSvc.stop();
+		try (BlobStore blobStore = LocalBlobStore.open(Paths.get(args[0]))) {
+
+			// virtual file system
+			VirtualFileSystem fs = new BlobStoreVFS(blobStore);
+
+			// exports config file
+			URL exportsUrl = Main.class.getClassLoader().getResource(
+					"doss/nfs/exports");
+			if (exportsUrl == null) {
+				throw new RuntimeException(
+						"doss/nfs/exports not found on classpath");
+			}
+			ExportFile exports = new ExportFile(exportsUrl);
+
+			// NFS 4 server
+			DeviceManager devManager = new DeviceManager();
+			MDSOperationFactory opfac = new MDSOperationFactory();
+			NfsIdMapping idMap = new SimpleIdMap();
+			NFSServerV41 nfs4 = new NFSServerV41(opfac, devManager, fs, idMap,
+					exports);
+
+			// mountd
+			MountServer mountd = new MountServer(exports, fs);
+
+			// RPC server
+			OncRpcSvc rpcSvc = new OncRpcSvcBuilder().withPort(1234).withTCP()
+					.build();
+			Map<OncRpcProgram, RpcDispatchable> services = new HashMap<>();
+			rpcSvc.register(new OncRpcProgram(100003, 4), nfs4);
+			rpcSvc.register(new OncRpcProgram(100005, 3), mountd);
+			rpcSvc.setPrograms(services);
+			rpcSvc.start();
+
+			try {
+				// hang around for a while
+				while (true) {
+					Thread.sleep(100000000);
+				}
+			} finally {
+				rpcSvc.stop();
+			}
 		}
 	}
 }
