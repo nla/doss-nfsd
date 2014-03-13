d9fc7a601d0c1164acc6f19b9e1a3e829cc94ef3
Alex Osborne <aosborne@nla.gov.au>
2014-03-13 11:59:41 +1100

    Pass to path to exports file on the command-line, reload with HUP

--- src/doss/nfs/Main.java
+++ src/doss/nfs/Main.java
@@ -1,6 +1,8 @@
 package doss.nfs;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -23,27 +25,35 @@ import org.dcache.xdr.RpcDispatchable;
 import doss.BlobStore;
 import doss.local.LocalBlobStore;
 
+import sun.misc.*;
+
 public class Main {
 	public static void main(String[] args) throws IOException, OncRpcException,
 			InterruptedException {
-		if (args.length < 1) {
-			System.err.println("Usage: doss-nfsd blobstore-path");
+		if (args.length < 2) {
+			System.err.println("Usage: doss-nfsd blobstore-path exports-file");
+			System.err.println("");
+			System.err.println("The exports file is the same format as /etc/exports.  Use / as the path.");
+			System.err.println("You can send SIGHUP to reload it without restarting.");
 			System.exit(1);
 		}
+		
+		String blobStorePath = args[0];
+		String exportsFilePath = args[1];
 
-		try (BlobStore blobStore = LocalBlobStore.open(Paths.get(args[0]))) {
+		try (BlobStore blobStore = LocalBlobStore.open(Paths.get(blobStorePath))) {
 
 			// virtual file system
 			VirtualFileSystem fs = new BlobStoreVFS(blobStore);
 
-			// exports config file
-			URL exportsUrl = Main.class.getClassLoader().getResource(
-					"doss/nfs/exports");
-			if (exportsUrl == null) {
-				throw new RuntimeException(
-						"doss/nfs/exports not found on classpath");
+			// exports config file			
+			ExportFile exports;
+			try {
+				exports = new ExportFile(new URL(exportsFilePath));
+			} catch (MalformedURLException e) {
+				exports = new ExportFile(new File(exportsFilePath));
 			}
-			ExportFile exports = new ExportFile(exportsUrl);
+			registerSignalHandler(exports);
 
 			// NFS 4 server
 			DeviceManager devManager = new DeviceManager();
@@ -73,5 +83,20 @@ public class Main {
 				rpcSvc.stop();
 			}
 		}
+	}
+
+	@SuppressWarnings("restriction")
+	private static void registerSignalHandler(final ExportFile exports) {
+		Signal.handle(new Signal("HUP"), new SignalHandler() {
+		    public void handle(Signal signal)
+		    {
+		        try {
+		        	System.out.println("HUP signal received, rescanning exports file.");
+		        	exports.rescan();
+				} catch (IOException e) {
+					throw new RuntimeException("Error reloading exports file", e);
+				}
+		    }
+		});
 	}
 }
